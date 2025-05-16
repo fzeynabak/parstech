7da8e461bc77d504482ae610827b05fc281021e0
akradimo <fzeynabak@gmail.com>
2025-05-16 23:16:22 +0330

    Create 2025_05_16_194539_add_currency_id_to_sales_table.php

---
+++ database/migrations/2025_05_16_194539_add_currency_id_to_sales_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->unsignedBigInteger('currency_id')->nullable()->after('seller_id');
+            $table->foreign('currency_id')->references('id')->on('currencies')->nullOnDelete();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->dropForeign(['currency_id']);
+            $table->dropColumn('currency_id');
+        });
+    }
+};
